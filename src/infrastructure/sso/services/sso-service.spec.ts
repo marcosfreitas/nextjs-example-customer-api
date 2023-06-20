3a6fefb0b987370445ce4c35c825a0f34eedd661
Marcos Freitas <marcosvsfreitas@gmail.com>
2023-06-20 14:04:41 -0300

    finished sso service unit tests

--- src/infrastructure/sso/services/sso-service.spec.ts
+++ src/infrastructure/sso/services/sso-service.spec.ts
@@ -8,7 +8,7 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { AxiosResponse, AxiosError } from 'axios';
 import { SSOService } from './sso.service';
-import { firstValueFrom, of, throwError } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { SSOUserInfoSuccessResponse } from '@infrastructure/sso/contracts/sso-user-info.response';
 
 describe('SSOService Unit Tests', () => {
@@ -79,25 +79,56 @@ describe('SSOService Unit Tests', () => {
     });
 
     it('should throw InternalServerErrorException if the request fails with other error status', async () => {
-      try {
-        const responseMock: AxiosResponse = {
-          data: {},
-          status: HttpStatus.BAD_REQUEST,
-          statusText: 'Bad Request',
-          headers: {},
-          config: {} as any,
-        };
+      const responseMock: AxiosResponse = {
+        data: {},
+        status: HttpStatus.BAD_REQUEST,
+        statusText: 'Bad Request',
+        headers: {},
+        config: {} as any,
+      };
 
-        jest
-          .spyOn(httpService, 'post')
-          .mockResolvedValueOnce(of(responseMock) as never);
+      jest
+        .spyOn(httpService, 'post')
+        .mockResolvedValueOnce(of(responseMock) as never);
 
-        await expect(ssoService.getUserInfo(bearerToken)).rejects.toThrow(
-          InternalServerErrorException,
-        );
-      } catch (error) {
-        console.log('error', error);
-      }
+      await expect(ssoService.getUserInfo(bearerToken)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
+
+    it('should throw BadGatewayException when the SSO server is unavailable', async () => {
+      const errorMock: AxiosError = {
+        code: 'ECONNREFUSED',
+        message: 'Connection refused',
+        config: {} as any,
+        request: {},
+        response: undefined,
+        isAxiosError: true,
+        toJSON: () => {
+          return {};
+        },
+        name: '',
+      };
+
+      jest
+        .spyOn(httpService, 'post')
+        .mockReturnValueOnce(throwError(() => errorMock));
+
+      await expect(ssoService.getUserInfo(bearerToken)).rejects.toThrow(
+        BadGatewayException,
+      );
+    });
+
+    it('should throw InternalServerErrorException for other unexpected authorization errors', async () => {
+      const errorMock: Error = new Error('Unexpected error');
+
+      jest
+        .spyOn(httpService, 'post')
+        .mockReturnValueOnce(throwError(() => errorMock));
+
+      await expect(ssoService.getUserInfo(bearerToken)).rejects.toThrow(
+        InternalServerErrorException,
+      );
     });
   });
 });
